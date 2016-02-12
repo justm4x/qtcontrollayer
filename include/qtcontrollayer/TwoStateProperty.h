4bc7f585b512ad3fbfcb1c3d704403a4733a3578
max <mlevdik@gmail.com>
2016-02-12 11:39:20 +0200

    TwoStateProperty

---
+++ include/qtcontrollayer/TwoStateProperty.h
@@ -0,0 +1,24 @@
+#include "QObjProperty.h"
+
+class TwoStateProperty : public QObjProperty
+{
+    Q_OBJECT
+    Q_PROPERTY(bool state READ state WRITE setState NOTIFY stateChanged);
+public:
+    explicit TwoStateProperty(const QString & name, const QString & displayName, bool state, const QStringList & strings, QObject * parent = 0);
+    virtual ~TwoStateProperty();
+
+public:
+    bool state() const;
+    void setState(bool state);
+
+    virtual QString displayValue() const override;
+
+signals:
+    void stateChanged();
+
+private:
+    bool _state;
+    bool _defaultState;
+    QStringList _strings;
+};
---
+++ src/Property/TwoStateProperty.cpp
@@ -0,0 +1,34 @@
+#include "../../include/qtcontrollayer/TwoStateProperty.h"
+
+
+TwoStateProperty::TwoStateProperty(const QString & name, const QString & displayName, bool state, const QStringList & strings, QObject * parent /*= 0*/)
+    : QObjProperty(name, displayName, parent)
+    , _state(state)
+    , _defaultState(state)
+    , _strings(strings)
+{
+    connect(this, &TwoStateProperty::stateChanged, this, &TwoStateProperty::changed);
+}
+
+TwoStateProperty::~TwoStateProperty()
+{
+}
+
+bool TwoStateProperty::state() const
+{
+    return _state;
+}
+
+void TwoStateProperty::setState(bool state)
+{
+    if (_state != state)
+    {
+        _state = state;
+        emit stateChanged();
+    }
+}
+
+QString TwoStateProperty::displayValue() const
+{
+    return _strings.isEmpty() ? "" : _strings[static_cast<int>(_state)];
+}
